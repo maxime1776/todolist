be6ba975f78ef55ea10ba0234e2f359285b9dfe7
Maxime Santilli <maxime1776@gmail.com>
2015-10-27 16:20:07 +0100

    program done

--- todolist/todolist/app/controllers/tasks_controller.rb
+++ todolist/todolist/app/controllers/tasks_controller.rb
@@ -1,2 +1,50 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+   @task = Task.find(params[:id])
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def update
+   @task = Task.find(params[:id])
+   @task.update(task_params)
+   redirect_to tasks_path
+
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.delete
+    redirect_to tasks_path
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :description, :detail, :priority)
+  end
+
+
+
+
 end
+
+ def destroy
+    @restaurant.delete
+    redirect_to restaurants_path
+  end
--- todolist/todolist/db/schema.rb
+++ todolist/todolist/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027102512) do
+ActiveRecord::Schema.define(version: 20151027131556) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151027102512) do
     t.datetime "updated_at",  null: false
     t.integer  "priority"
     t.string   "description"
+    t.string   "details"
   end
 
 end
